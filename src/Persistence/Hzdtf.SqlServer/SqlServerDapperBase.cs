c7c48c305d0c6fbab311757ce383c65fd30ca2c6
hzdtf <hzdtf@PC-HUANGZHENDON>
2022-07-06 07:29:46 +0800

    sqlserver批量插入时更改为SqlBulkCopy

--- src/Persistence/Hzdtf.SqlServer/SqlServerDapperBase.cs
+++ src/Persistence/Hzdtf.SqlServer/SqlServerDapperBase.cs
@@ -1,9 +1,11 @@
 ﻿using Hzdtf.Persistence.Dapper;
 using Hzdtf.Utility.Model;
+using Hzdtf.Utility.Utils;
 using Microsoft.Data.SqlClient;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Linq;
 using System.Text;
 
 namespace Hzdtf.SqlServer
@@ -29,9 +31,87 @@ namespace Hzdtf.SqlServer
         /// </summary>
         protected override string SufxEscapeChar { get => "]"; }
 
+        /// <summary>
+        /// 同步插入的字段映射类型字典
+        /// </summary>
+        private static readonly object syncInsertFieldMapTypes = new object();
+
+        /// <summary>
+        /// 插入的字段映射类型字典
+        /// </summary>
+        private static IDictionary<string, Type> insertFieldMapTypes;
+
+        /// <summary>
+        /// 插入的字段映射类型字典
+        /// </summary>
+        private IDictionary<string, Type> InsertFieldMapTypes
+        {
+            get
+            {
+                if (insertFieldMapTypes == null)
+                {
+                    var allFields = InsertFieldNames();
+                    lock (syncInsertFieldMapTypes)
+                    {
+                        insertFieldMapTypes = new Dictionary<string, Type>(allFields.Length);
+                        var type = typeof(ModelT);
+                        foreach (var field in allFields)
+                        {
+                            var prop = type.GetProperty(GetPropByField(field));
+                            insertFieldMapTypes.Add(field, prop.PropertyType);
+                        }
+                    }
+                }
+
+                return insertFieldMapTypes;
+            }
+
+        }
+
         #endregion
 
         #region 重写父类的方法
+
+        /// <summary>
+        /// 插入模型列表
+        /// </summary>
+        /// <param name="models">模型列表</param>
+        /// <param name="dbConnection">数据库连接</param>
+        /// <param name="propertyNames">属性名称集合</param>
+        /// <param name="dbTransaction">数据库事务</param>
+        /// <param name="comData">通用数据</param>
+        /// <returns>影响行数</returns>
+        protected override int Insert(IList<ModelT> models, IDbConnection dbConnection, string[] propertyNames = null, IDbTransaction dbTransaction = null, CommonUseData comData = null)
+        {
+            return ExecRecordSqlLog<int>("SqlBulkCopy", () =>
+            {
+                if (IsSupportIdempotent)
+                {
+                    try
+                    {
+                        return ExecSqlBulkCopy(models, dbConnection, propertyNames, dbTransaction);
+                    }
+
+                    catch (Exception ex)
+                    {
+                        // 如果主键重复，则忽略异常，实现幂等
+                        if (IsExceptionPkRepeat(ex))
+                        {
+                            var ids = models.Select(p => p.Id).ToArray();
+                            Log.InfoAsync($"批量插入.发生主键重复异常,幂等操作忽略该异常.主键:{ids.ToMergeString(",")}", ex, this.GetType().Name, comData.GetEventId(), "Insert", "SqlBulkCopy");
+
+                            return models.Count;
+                        }
+
+                        throw new Exception(ex.Message, ex);
+                    }
+                }
+                else
+                {
+                    return ExecSqlBulkCopy(models, dbConnection, propertyNames, dbTransaction);
+                }
+            }, comData: comData, "Insert", "SqlBulkCopy");
+        }
 
         /// <summary>
         /// 创建数据库连接
@@ -49,7 +129,7 @@ namespace Hzdtf.SqlServer
         protected override string GetPartPageSql(int pageIndex, int pageSize)
         {
             var start = pageIndex > 0 ? pageIndex * pageSize : pageIndex;
-            return $"OFFSET {start} rows fetch next {pageSize} rows only";
+            return $"OFFSET {start} ROWS FETCH NEXT {pageSize} ROWS ONLY";
         }
 
         /// <summary>
@@ -117,12 +197,57 @@ namespace Hzdtf.SqlServer
         }
 
         /// <summary>
-        /// 其他判断主键重复，此方法目的是为了异常可能包含其他非主键重复的
-        /// 如果子类没重写，默认为是
+        /// 执行批量插入
+        /// 会拿模型第1条数据作为Id作为默认值
         /// </summary>
-        /// <param name="ex">异常</param>
-        /// <returns>其他判断主键重复</returns>
-        protected virtual bool OtherIsPkRepeat(SqlException ex) => true;
+        /// <param name="models">模型列表</param>
+        /// <param name="dbConnection">数据库连接</param>
+        /// <param name="propertyNames">属性名称数组</param>
+        /// <param name="dbTransaction">数据库事务</param>
+        /// <returns>影响行数</returns>
+        protected int ExecSqlBulkCopy(IList<ModelT> models, IDbConnection dbConnection, string[] propertyNames = null, IDbTransaction dbTransaction = null)
+        {
+            var defaultId = models[0].Id;
+            var trans = dbTransaction == null ? null : dbTransaction as SqlTransaction;
+            var entityType = typeof(ModelT);
+            using (var bulkCopy = new SqlBulkCopy(dbConnection as SqlConnection, SqlBulkCopyOptions.TableLock, trans))
+            {
+                bulkCopy.BatchSize = 100000;
+                bulkCopy.BulkCopyTimeout = 120;
+                bulkCopy.DestinationTableName = Table;
+
+                SetSqlBulkCopy(bulkCopy);
+
+                // 需要插入的字段数组
+                var insertFields = WrapInsertFieldNames(defaultId, propertyNames);
+                var table = new DataTable();
+
+                // 循环需要插入的数组，并添加到对应的内存表里
+                foreach (var field in insertFields)
+                {
+                    var fieldType = InsertFieldMapTypes[field]; // 字段类型
+
+                    bulkCopy.ColumnMappings.Add(field, field);
+                    table.Columns.Add(field, fieldType);
+                }
+
+                // 循环模型列表，添加内存行并设置单元格数据
+                foreach (var item in models)
+                {
+                    var row = table.NewRow();
+                    foreach (var field in insertFields)
+                    {
+                        row[field] = entityType.GetProperty(GetPropByField(field)).GetValue(item);
+                    }
+
+                    table.Rows.Add(row);
+                }
+
+                bulkCopy.WriteToServer(table);
+            }
+
+            return models.Count;
+        }
 
         /// <summary>
         /// 获取不锁表的SQL
@@ -131,6 +256,32 @@ namespace Hzdtf.SqlServer
         /// <param name="alias">别名</param>
         /// <returns>不锁表的SQL</returns>
         protected override string GetNoLockTableSql(string table, string alias = null) => $"{table} {alias} WITH(NOLOCK)";
+
+        #endregion
+
+        #region 私有方法
+
+
+        #endregion
+
+        #region 虚方法
+
+        /// <summary>
+        /// 设置SQL Bulk Copy
+        /// 默认每次批量插入10万条，超时2分钟。如果要设置不同，请在此方法里设置
+        /// </summary>
+        /// <param name="bulk">SQL Bulk Copy</param>
+        protected virtual void SetSqlBulkCopy(SqlBulkCopy bulk)
+        {
+        }
+
+        /// <summary>
+        /// 其他判断主键重复，此方法目的是为了异常可能包含其他非主键重复的
+        /// 如果子类没重写，默认为是
+        /// </summary>
+        /// <param name="ex">异常</param>
+        /// <returns>其他判断主键重复</returns>
+        protected virtual bool OtherIsPkRepeat(SqlException ex) => true;
 
         #endregion
     }
